4fe6271ef92e4a2c3e7ebe7f0497f486fe522451
Vadim <vadim.podogov2003@gmail.com>
2023-06-03 22:04:30 +0300

    initial

---
+++ src/main/java/com/example/TGbotMoney/TGbotMoneyApplication.java
@@ -0,0 +1,13 @@
+package com.example.TGbotMoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TGbotMoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TGbotMoneyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TGbotMoney/TGbotMoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TGbotMoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TGbotMoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
